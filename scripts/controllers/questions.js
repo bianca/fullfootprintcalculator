a314a60d4047c4cc9e5432180715eb45ab66f6db
bianca <bianca.sayan@gmail.com>
2017-11-21 21:46:10 -0500

    angular routes fix

--- scripts/controllers/questions.js
+++ scripts/controllers/questions.js
@@ -28,7 +28,7 @@ angular.module('ffpApp')
 	    	$scope.currentquestion = $rootScope.questions[$scope.pointer]
         $scope.fl = !$scope.fl
 	    } else {
-	    	$window.location.url = $rootScope.localhref+"results"
+	    	$window.location.href = $rootScope.localhref+"results"
 	    }
     }
 
